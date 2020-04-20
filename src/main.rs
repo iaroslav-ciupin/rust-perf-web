9b0ff72ba7d5f286b39ee2fab538464a2aa46ed9
Iaroslav Ciupin <iaroslav.ciupin@ellation.com>
2020-04-20 19:39:22 +0300

    compute func

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,29 @@
 use actix_web::{get, web, App, HttpServer, Responder};
 use rand::Rng;
+use std::f64::consts::PI;
+use std::convert::TryFrom;
 
-fn compute(n: usize) -> f64 {
-    let mut arr = vec![0.0; n];
-    let mut sum = 0.0;
-    for i in 0..n {
-        arr[i] = rand::thread_rng().gen();
-        sum += arr[i]
+fn compute(n: u64) -> f64 {
+    let mut rng = rand::thread_rng();
+    let size = usize::try_from(n).unwrap();
+    let mut arr = vec![0.0; size];
+    for i in arr.iter_mut() {
+        *i = rng.gen::<f64>();
     }
-    sum
+    let mut sum: f64 = 0.0;
+    let mut sin_sum: f64 = 0.0;
+    for i in arr.into_iter() {
+        sum += i;
+        sin_sum += (2.0 * PI * i).sin();
+    }
+    // println!("Sum {}", sum);
+    // println!("Sin sum {}", sin_sum);
+    sum / sin_sum
 }
 
 #[get("/work/{n}")]
-async fn index(info: web::Path<usize>) -> impl Responder {
-    let n: usize = info.into_inner();
+async fn index(info: web::Path<u64>) -> impl Responder {
+    let n: u64 = info.into_inner();
     println!("Received work: {}", n);
     format!("{}", compute(n))
 }
