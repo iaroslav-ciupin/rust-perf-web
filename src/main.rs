6616ebaf1c82f377c366bde0c0b8ded205359934
Iaroslav Ciupin <iaroslav.ciupin@ellation.com>
2020-04-21 15:47:46 +0300

    adjust compute

--- src/main.rs
+++ src/main.rs
@@ -3,29 +3,29 @@ use rand::Rng;
 use std::f64::consts::PI;
 use std::convert::TryFrom;
 
-fn compute(n: u64) -> f64 {
+fn compute(mem_size: u64, cpu_size: u64) -> f64 {
     let mut rng = rand::thread_rng();
-    let size = usize::try_from(n).unwrap();
-    let mut arr = vec![0.0; size];
+
+    // memory work
+    let arr_size = usize::try_from(mem_size).unwrap();
+    let mut arr = vec![0.0; arr_size];
     for i in arr.iter_mut() {
         *i = rng.gen::<f64>();
     }
+
+    // cpu work
+    let max_index = usize::try_from(cpu_size).unwrap();
     let mut sum: f64 = 0.0;
-    let mut sin_sum: f64 = 0.0;
-    for i in arr.into_iter() {
-        sum += i;
-        sin_sum += (2.0 * PI * i).sin();
+    for i in 0..max_index {
+        sum += (2.0 * PI * arr[i]).sin();
     }
-    // println!("Sum {}", sum);
-    // println!("Sin sum {}", sin_sum);
-    sum / sin_sum
+    sum
 }
 
-#[get("/work/{n}")]
-async fn index(info: web::Path<u64>) -> impl Responder {
-    let n: u64 = info.into_inner();
+#[get("/work/{mem}/{cpu}")]
+async fn index(info: web::Path<(u64, u64)>) -> impl Responder {
     //println!("Received work: {}", n);
-    format!("{}", compute(n))
+    format!("{}", compute(info.0, info.1))
 }
 
 #[actix_rt::main]
